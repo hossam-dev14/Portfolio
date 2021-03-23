630f0bfc73919b1314caa73de439026b7e9426cc
Hossam Dev <73648971+hossam-dev14@users.noreply.github.com>
2021-03-24 00:22:47 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,11 @@
 const toggle = document.querySelector('.toggle');
 const nav = document.querySelector('nav');
 const ul = nav.childNodes[5];
-const li = document.querySelectorAll('li');
-const links = document.querySelectorAll('li a');
+const links = document.querySelectorAll('li');
 const logo = document.querySelector('.logo');
 
 
 // Links add active class
-
 links.forEach((link) => {
     link.addEventListener('click', (e) => {
 
@@ -22,97 +20,60 @@ links.forEach((link) => {
 });
 
 
-// toggle bar
+// Toggle bar
 toggle.addEventListener('click', function(){
     'use strict;'
   
     this.classList.toggle('active-nav');
     nav.classList.toggle('active-nav');
     logo.classList.toggle('active-logo');
-    
-    if (li[0].style.transform !== 'translateX(0%)') {
-        setTimeout(() => {
-            li.forEach(e => {
-                e.style.transform = 'translateX(0%)';
-                e.style.transitionDuration = '1.3s';
-            });
-        }, 50);
-    } 
-    else {
-        li.forEach(e => {
-           // e.style.transform = 'translateX(560%)';
+});
 
-        }); 
+
+
+  // Smoth Scroll
+  function goToID(id) {
+      document.querySelector(id).scrollIntoView({
+          behavior: 'smooth'
+      });
+  }
+
+
+  // ScrollUp
+  let scrollUp = document.querySelector('.scrollup');
+  // Show & Hide TOP button
+  window.onscroll = function () {
+    'use strict';
+
+    if (window.pageYOffset >= 600) {
+        scrollUp.style.display = 'block';
+    } else {
+        scrollUp.style.display = 'none'
     }
-});
+  }
+
+  // Scroll to 0px from the TOP
+  function goToPixel(pix) {
+      window.scroll({
+        top: pix,
+        left: 0,
+        behavior: 'smooth'
+      });
+  }
+
+  scrollUp.addEventListener("click", function goToPixel(pix) {
+      pix = 0;
+      window.scroll({
+        top: pix,
+        left: 0,
+        // top: 0,
+        behavior: 'smooth'
+      });
+    }
+  );
 
 
-
-
-// document.body.onresize = function() {
-//     li.forEach(e => {
-//         e.style.transform = 'translateX(0%)';
-//     });
-// }
-
-// const home = document.querySelector('#home');
-// home.addEventListener('click', function(){
-//     'use strict;'
-//     if (this.classList == 'active') {
-
-//     }
-//     this.classList.remove('active');
-//     nav.classList.remove('active');
-// });
-
-
-// Change color
-// document.documentElement.style.setProperty(`--primary`, '#1f1');
-
-
-const red = document.querySelector('#red');
-red.addEventListener('click', e =>{
-    const cssStyle = document.documentElement.style;
-    cssStyle.setProperty('--primary', '#e00');
-    cssStyle.setProperty('--primary-9', '#ffc2c2');
-});
-
-const aqua = document.querySelector('#aqua');
-aqua.addEventListener('click', e =>{
-    const cssStyle = document.documentElement.style;
-    cssStyle.setProperty('--primary', '#0ff');
-    cssStyle.setProperty('--primary-9', '#dff');
-});
-
-const fuchsia = document.querySelector('#fuchsia');
-fuchsia.addEventListener('click', e =>{
-    const cssStyle = document.documentElement.style;
-    cssStyle.setProperty('--primary', '#f0f');
-    cssStyle.setProperty('--primary-9', '#fdf');
-});
-
-const teal = document.querySelector('#teal');
-teal.addEventListener('click', e =>{
-    const cssStyle = document.documentElement.style;
-    cssStyle.setProperty('--primary', '#008080');
-    cssStyle.setProperty('--primary-9', '#85b8b8');
-});
-
-const springgreen = document.querySelector('#springgreen');
-springgreen.addEventListener('click', e =>{
-    const cssStyle = document.documentElement.style;
-    cssStyle.setProperty('--primary', '#00ff7f');
-    cssStyle.setProperty('--primary-9', '#cfe');
-});
-
-
-
-
-
-
-
-
-
+    
 
 
 
